e70747742716f5f83ab263137f8305a9158d0c27
DylanQL <angelo.quispe.l@tecsup.edu.pe>
2025-10-30 19:45:42 -0500

    Agregar archivos de proyecto.

---
+++ Hugo_Torrico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hugo_Torrico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
